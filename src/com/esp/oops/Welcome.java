45a60a33cb4a8f3c7f2cd9c57de616f9e6fedb4b
sprasad786 <sprasad786@users.noreply.github.com>
2015-11-21 11:33:33 +0530

    Developer name:sp
    Reviewer Name:sp
    
    comments:Added sample java

---
+++ src/com/esp/oops/Welcome.java
@@ -0,0 +1,17 @@
+package com.esp.oops;
+
+ /* This class is welcome to java
+  * @author siva
+  */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java");
+
+	}
+
+}
